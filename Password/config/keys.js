241d8efa49c430cb96cd0ab28cf7d3b06713cdf7
BrianJ-Code <55200248+BrianJ-Code@users.noreply.github.com>
2020-02-02 23:29:47 -0500

    1

---
+++ Password/config/keys.js
@@ -0,0 +1,3 @@
+dbPassword = 'mongodb+srv://admin:'+ encodeURIComponent('OmZDczLYB89Or5TF') + '@cluster0-t5ctx.mongodb.net/test?retryWrites=true&w=majority';
+
+module.exports = {mongoURI: dbPassword};
